911d8d6814c328c85602d60808639211e1fd52e3
Brett Morin <brettmorin2@gmail.com>
2016-02-11 10:09:38 -0800

    Changed Service to store data in a database

--- ChartingService/ChartingService.vb
+++ ChartingService/ChartingService.vb
@@ -7,34 +7,35 @@ Public Class ChartingService
   Private _timer As Timers.Timer = New Timers.Timer()
   Private _sqlTalker As DataAccess.SQLTalker
   Private _eventId As Integer
-  Private _pollingDurationInMinutes As Integer
+  Private _pollingDurationInMinutes As Integer = 1
   Private _chartingAPIProviderType As ChartingProviderAPIType
   Private _serviceStatus As ServiceStatus = New ServiceStatus()
+
+  Public Enum ServiceState
+    SERVICE_STOPPED = 1
+    SERVICE_START_PENDING = 2
+    SERVICE_STOP_PENDING = 3
+    SERVICE_RUNNING = 4
+    SERVICE_CONTINUE_PENDING = 5
+    SERVICE_PAUSE_PENDING = 6
+    SERVICE_PAUSED = 7
+  End Enum
+
+  <StructLayout(LayoutKind.Sequential)>
+  Public Structure ServiceStatus
+    Public dwServiceType As Long
+    Public dwCurrentState As ServiceState
+    Public dwControlsAccepted As Long
+    Public dwWin32ExitCode As Long
+    Public dwServiceSpecificExitCode As Long
+    Public dwCheckPoint As Long
+    Public dwWaitHint As Long
+  End Structure
 
   Declare Auto Function SetServiceStatus Lib "advapi32.dll" (ByVal handle As IntPtr, ByRef serviceStatus As ServiceStatus) As Boolean
 
   Public Sub New(ByVal cmdArgs() As String)
     InitializeComponent()
-  End Sub
-
-  Private Sub SetUpLoggingEvent()
-    Dim eventSourceName As String = "ChartingSource"
-    Dim logName As String = "ChartingLog"
-
-    _chartingEventLog = New EventLog()
-    If (Not EventLog.SourceExists(eventSourceName)) Then EventLog.CreateEventSource(eventSourceName, logName)
-
-    _chartingEventLog.Source = eventSourceName
-    _chartingEventLog.Log = logName
-  End Sub
-
-  Private Sub SetStatus(status As ServiceState, Optional waitHint As Integer = 0)
-    _serviceStatus.dwCurrentState = ServiceState.SERVICE_START_PENDING
-
-    If (waitHint > 0) Then
-      _serviceStatus.dwWaitHint = waitHint
-    End If
-    SetServiceStatus(ServiceHandle, _serviceStatus)
   End Sub
 
   Protected Overrides Sub OnStart(ByVal args() As String)
@@ -65,7 +66,7 @@ Public Class ChartingService
 
 
     _timer.Interval = _pollingDurationInMinutes * 10000 'CHANGE THIS LATER 60000
-    AddHandler _timer.Elapsed, AddressOf OnTimer
+    AddHandler _timer.Elapsed, AddressOf UpdateDatabaseWithProviderValues
     _timer.Start()
 
     ' Update the service state to Running.
@@ -79,35 +80,59 @@ Public Class ChartingService
     SetStatus(ServiceState.SERVICE_STOPPED)
   End Sub
 
-  Private Sub OnTimer(sender As Object, e As Timers.ElapsedEventArgs)
-    Const sqlCommand As String = "EXEC Ships.pInsertOrUpdateShipPosition 1, 111111111, 'Anne Sleuth', 46.851859, -129.322418, 1"
-    Dim countOfShips = _sqlTalker.Procer(sqlCommand)
-    Dim outputMessage = $"Provider Type: {_chartingAPIProviderType} with {countOfShips}"
+  Private Sub UpdateDatabaseWithProviderValues(sender As Object, e As Timers.ElapsedEventArgs)
+    Dim outputMessage = String.Empty
+    Dim data As IList(Of ShipDb) = New List(Of ShipDb)
 
-    _chartingEventLog.WriteEntry(outputMessage, EventLogEntryType.Information, _eventId)
+    Try
+      data = ReturnShipsFromProvider(_chartingAPIProviderType, 0.00001)
+      outputMessage += $"Count of Ships to insert {data.Count} {Environment.NewLine}"
+    Catch ex As Exception
+      'TODO: email out someone that we could not get data
+      outputMessage += $"Could not get a list of the ships!"
+    End Try
+
+    data.ToList().ForEach(Sub(x)
+                            Dim sqlCommand = $"EXEC Ships.pInsertOrUpdateShipPosition 
+                              @Incremenet = {_pollingDurationInMinutes},
+                              @MMSI = {x.MMSI}, 
+                              @ShipName = '{x.ShipName}', 
+                              @Latitude = {x.Latitude}, 
+                              @Longitude = {x.Longitude}, 
+                              @ShipTypeId = {x.ShipTypeId}"
+
+                            Try
+                              Dim result = _sqlTalker.Procer(sqlCommand)
+                              outputMessage += $"\tMMSI :  {x.MMSI} successfully inserted! {Environment.NewLine}"
+                            Catch ex As Exception
+                              outputMessage += $"\tMMSI: {x.MMSI} could not be inserted! {Environment.NewLine}"
+                            End Try
+
+                          End Sub)
+
+    _chartingEventLog.WriteEntry(outputMessage, EventLogEntryType.Information, _eventId + 1)
     _eventId += 1
   End Sub
 
-  Public Enum ServiceState
-    SERVICE_STOPPED = 1
-    SERVICE_START_PENDING = 2
-    SERVICE_STOP_PENDING = 3
-    SERVICE_RUNNING = 4
-    SERVICE_CONTINUE_PENDING = 5
-    SERVICE_PAUSE_PENDING = 6
-    SERVICE_PAUSED = 7
-  End Enum
+  Private Sub SetUpLoggingEvent()
+    Dim eventSourceName As String = "ChartingSource"
+    Dim logName As String = "ChartingLog"
 
-  <StructLayout(LayoutKind.Sequential)>
-  Public Structure ServiceStatus
-    Public dwServiceType As Long
-    Public dwCurrentState As ServiceState
-    Public dwControlsAccepted As Long
-    Public dwWin32ExitCode As Long
-    Public dwServiceSpecificExitCode As Long
-    Public dwCheckPoint As Long
-    Public dwWaitHint As Long
-  End Structure
+    _chartingEventLog = New EventLog()
+    If (Not EventLog.SourceExists(eventSourceName)) Then EventLog.CreateEventSource(eventSourceName, logName)
+
+    _chartingEventLog.Source = eventSourceName
+    _chartingEventLog.Log = logName
+  End Sub
+
+  Private Sub SetStatus(status As ServiceState, Optional waitHint As Integer = 0)
+    _serviceStatus.dwCurrentState = ServiceState.SERVICE_START_PENDING
+
+    If (waitHint > 0) Then
+      _serviceStatus.dwWaitHint = waitHint
+    End If
+    SetServiceStatus(ServiceHandle, _serviceStatus)
+  End Sub
 
   'UserService overrides dispose to clean up the component list.
   <DebuggerNonUserCode()>
